fd01ee304be5a23341c3e1f9f89b32d17cfbf568
nithinj95 <nithingowda214@gmail.com>
2023-08-20 13:08:28 +0530

    file path removed

--- pingtest/ping_test.sh
+++ pingtest/ping_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Define the path to the text file containing the list of devices
-devices_file="/homes/nithinj/script/pingtest/devices.txt"
+devices_file="path to devices_file"
 
 # Define the path to the file where the list of pingable devices will be stored
-pingable_devices="/homes/nithinj/script/pingtest/pingable_devices.txt"
+pingable_devices="change the path"
 
 # Define the path to the file where the list of non-pingable devices will be stored
-non_pingable_devices="/homes/nithinj/script/pingtest/non_pingable_devices.txt"
+non_pingable_devices="change the path"
 
 # Read the list of devices from the text file
 devices=($(cat "$devices_file"))
